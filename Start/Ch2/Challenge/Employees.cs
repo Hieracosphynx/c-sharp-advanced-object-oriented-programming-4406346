88b953d3f6aeea64f5f15f0e34372987dda34e03
Miguel Cortez <44310973+Hieracosphynx@users.noreply.github.com>
2024-10-25 03:33:39 +0000

    Ch1 & Ch2

--- Start/Ch2/Challenge/Employees.cs
+++ Start/Ch2/Challenge/Employees.cs
@@ -1,12 +1,74 @@
 // Example file for Advanced C#: Object Oriented Programming by Joe Marini
 // Solution to the Employee Class challenge
 
+
+
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices;
+
+
+/**
+* AdjustPay
+* Required:
+* ID
+* Department
+* Full Name
+*/
 public class Employee {
     public Employee() {}
 
     // YOUR CODE GOES HERE
+    public virtual void AdjustPay(decimal dec) { }
+
+    public required int ID {
+        get;
+        init;
+    }
+
+    public required string Department {
+        get;
+        set;
+    }
+
+    public required string FirstName {
+        get;
+        set;
+    }
+
+    public required string LastName {
+        get;
+        set;
+    }
+
+    public string FullName {
+        get { return $"{FirstName} {LastName}"; }
+    }
 
     public override string ToString() => $"{ID}:{FullName}, {Department} ";
 }
 
-// DEFINE OTHER CLASSES HERE
+public class HourlyEmployee : Employee {
+    public override void AdjustPay(decimal dec)
+    {
+        var product = PayRate * dec;
+        PayRate += product;
+    }
+
+    public decimal PayRate {
+        get;
+        set;
+    }
+}
+
+public class SalariedEmployee : Employee {
+    public override void AdjustPay(decimal dec)
+    {
+        var product = Salary * dec;
+        Salary += product;
+    }
+
+    public decimal Salary {
+        get;
+        set;
+    }
+}
--- Start/Ch2/Challenge/Program.cs
+++ Start/Ch2/Challenge/Program.cs
@@ -2,8 +2,8 @@
 // Challenge: create a class hierarchy to represent employees
 
 // Test code for the programming challenge
-var Emp1 = new HourlyEmployee() {FullName="John Doe", Department="Sales", ID=1, PayRate=35.0m};
-var Emp2 = new SalariedEmployee() {FullName="Jane Deaux", Department="Marketing", ID=2, Salary=75000.0m};
+var Emp1 = new HourlyEmployee() {FirstName="John", LastName="Doe", Department="Sales", ID=1, PayRate=35.0m};
+var Emp2 = new SalariedEmployee() {FirstName="Jane", LastName="Deaux", Department="Marketing", ID=2, Salary=75000.0m};
 
 Console.WriteLine($"{Emp1} -- {Emp1.PayRate:C2}");
 Emp1.Department = "Legal";
@@ -16,3 +16,6 @@ Console.WriteLine($"{Emp2} -- {Emp2.Salary:C2}");
 Emp2.Department = "Solution Architecture";
 Emp2.AdjustPay(0.07m);
 Console.WriteLine($"{Emp2} -- {Emp2.Salary:C2}");
+
+Emp2.AdjustPay(0.07m);
+Console.WriteLine($"aA{Emp2} -- {Emp2.Salary:C2}");
